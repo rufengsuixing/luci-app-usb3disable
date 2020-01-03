3294ac82d0a1b8a4a77fb9207dfb9c62f7eab54d
rufengsuixing <1985151814@qq.com>
2020-01-03 17:18:33 +0800

    first

---
+++ luasrc/model/cbi/usb3disable.lua
@@ -0,0 +1,37 @@
+require("luci.sys")
+local m,s,o
+local fs=require"nixio.fs"
+m = Map("usb3disable", "AdGuard Home")
+m.description = translate("Free and open source, powerful network-wide ads & trackers blocking DNS server.")
+m:section(SimpleSection).template  = "usb3disable/usb3disable_status"
+
+s = m:section(TypedSection, "usb3disable")
+s.anonymous=true
+s.addremove=false
+
+o=s:option(Button,"disabled",translate("disable now"))
+o.optional = true
+o.inputtitle=translate("Disable")
+o.write=function()
+	luci.sys.exec("cat /etc/modules.d/$(ls /etc/modules.d | grep usb3) | sed -n '1!G;h;$p' | xargs rmmod")
+	luci.http.redirect(luci.dispatcher.build_url("admin","system","usb3disable"))
+end
+
+if luci.sys.call("ls /etc/modules-boot.d | grep usb3")==0 then
+	o=s:option(Button,"disabledstartup",translate("disable on boot"))
+	o.optional = true
+	o.inputtitle=translate("Disable")
+	o.write=function()
+		luci.sys.exec("rm /etc/modules-boot.d/*-usb3")
+		luci.http.redirect(luci.dispatcher.build_url("admin","system","usb3disable"))
+	end
+else
+	o=s:option(Button,"enabledstartup",translate("enable on boot"))
+	o.optional = true
+	o.inputtitle=translate("Enable")
+	o.write=function()
+		luci.sys.exec("ln -s /etc/modules.d/*usb3 /etc/modules-boot.d/")
+		luci.http.redirect(luci.dispatcher.build_url("admin","system","usb3disable"))
+	end
+end
+return m
